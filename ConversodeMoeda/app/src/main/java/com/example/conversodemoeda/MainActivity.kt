a0f7805863232676138de6a70bb0919db3e1367d
Wandson Emanuel <107449320+Wuel1@users.noreply.github.com>
2023-06-22 15:32:14 -0300

    Conversor de Moeda c/ LinearLayout

---
+++ ConversodeMoeda/app/src/main/java/com/example/conversodemoeda/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.conversodemoeda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.conversodemoeda.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.buttonConverter.setOnClickListener{
+            if(binding.editTexto.text.toString().isEmpty()){
+                binding.textResultado.setText("Nenhum Valor Inserido")
+            }else{
+                val real = binding.editTexto.text.toString().toDouble()
+                val dolar = real * 4.75
+                val resultado = String.format("%.2f", dolar)
+                binding.textResultado.setText("${resultado} $")
+            }
+
+        }
+
+
+    }
+}
