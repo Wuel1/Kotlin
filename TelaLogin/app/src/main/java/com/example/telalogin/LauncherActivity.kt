d4762260edd4e5d7ea28674816608f0b36f98ee7
Wandson Emanuel <107449320+Wuel1@users.noreply.github.com>
2023-06-26 09:49:18 -0300

    Tela de Loading

---
+++ TelaLogin/app/src/main/java/com/example/telalogin/LauncherActivity.kt
@@ -0,0 +1,20 @@
+package com.example.telalogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class LauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },1000)
+    }
+
+}
