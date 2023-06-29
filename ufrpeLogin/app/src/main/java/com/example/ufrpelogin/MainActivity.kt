c306efa73a7aeb667c4187e17b3f4f28bb0cb1b7
Wandson Emanuel <107449320+Wuel1@users.noreply.github.com>
2023-06-29 15:46:47 -0300

    Sprint 1.0 | Fazendo a lógica do Login

--- ufrpeLogin/app/src/main/java/com/example/ufrpelogin/MainActivity.kt
+++ ufrpeLogin/app/src/main/java/com/example/ufrpelogin/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.ufrpelogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ufrpelogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding =  ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.buttonEntrar.setOnClickListener {
+            if(conferir(binding.buttonAluno))
+        }
+
     }
 }
