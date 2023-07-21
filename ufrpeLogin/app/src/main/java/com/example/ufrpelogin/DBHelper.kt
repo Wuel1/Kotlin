60ac8e273087db52e36a30070ca32d2e805c1412
Wandson Emanuel <107449320+Wuel1@users.noreply.github.com>
2023-07-21 11:38:06 -0300

    Sprint 2.0 | Começando a criar tabelas

--- ufrpeLogin/app/src/main/java/com/example/ufrpelogin/DBHelper.kt
+++ ufrpeLogin/app/src/main/java/com/example/ufrpelogin/DBHelper.kt
@@ -5,11 +5,20 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DBHelper(context: Context): SQLiteOpenHelper(context, "frequenciadatabase.db",null,1) {
-    override fun onCreate(p0: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+
+    val sql = arrayOf(
+        "CREATE TABLE professor (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, mac TEXT)" ,
+        "INSERT INTO professor (username,password,mac) VALUES ('waldemar','12345','00:45:E2:6A:46:3C')" ,
+    )
+
+    override fun onCreate(db: SQLiteDatabase) {
+        sql.forEach {
+            db.execSQL(it)
+        }
     }
 
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("Not yet implemented")
+    override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
+        db.execSQL("DROP TABLE professor")
+        onCreate(db)
     }
 }
