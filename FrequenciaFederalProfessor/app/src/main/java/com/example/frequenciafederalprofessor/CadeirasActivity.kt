cd7dff4c82858d3d5d75a3bc8f50596998dda50d
Wandson Emanuel <107449320+Wuel1@users.noreply.github.com>
2023-08-07 09:21:57 -0300

    Sprint 3.0 | Tela de Cadeiras

--- FrequenciaFederalProfessor/app/src/main/java/com/example/frequenciafederalprofessor/CadeirasActivity.kt
+++ FrequenciaFederalProfessor/app/src/main/java/com/example/frequenciafederalprofessor/CadeirasActivity.kt
@@ -1,7 +1,10 @@
 package com.example.frequenciafederalprofessor
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
 import com.example.frequenciafederalprofessor.databinding.ActivityCadeirasBinding
 
 class CadeirasActivity : AppCompatActivity() {
@@ -12,6 +15,19 @@ class CadeirasActivity : AppCompatActivity() {
         binding = ActivityCadeirasBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val webView = binding.webview
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.loadUrl("https://sigs.ufrpe.br/sigaa/verTelaLogin.do")
+
+        binding.addTurma.setOnClickListener {
+            webView.loadUrl("https://netuh.github.io/aed")
+        }
+        binding.removeTurma.setOnClickListener {
+            webView.loadUrl("https://www.google.com.br")
+        }
 
         binding.buttonVoltar.setOnClickListener {// Voltar
             finish()
