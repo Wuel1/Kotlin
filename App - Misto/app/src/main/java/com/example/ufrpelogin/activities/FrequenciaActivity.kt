e17cb6f889ac50275a6f12074e96c5f0d4964ef7
Wandson Emanuel <107449320+Wuel1@users.noreply.github.com>
2023-09-12 00:45:51 -0300

    Sprint Final | Verificando Direto no Firebase

--- App - Misto/app/src/main/java/com/example/ufrpelogin/activities/FrequenciaActivity.kt
+++ App - Misto/app/src/main/java/com/example/ufrpelogin/activities/FrequenciaActivity.kt
@@ -10,12 +10,19 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.ufrpelogin.databinding.ActivityFrequenciaAlunoBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -23,16 +30,25 @@ class FrequenciaActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFrequenciaAlunoBinding
     private lateinit var bluetoothAdapter: BluetoothAdapter
+    private lateinit var dbRef: DatabaseReference
+
 
     private val REQUEST_BLUETOOTH_PERMISSION = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val database =  FirebaseDatabase.getInstance()
+        dbRef = database.getReference("PROFESSOR")
         binding = ActivityFrequenciaAlunoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         bluetoothAdapter = bluetoothManager.adapter
+        try {
+            listPeriodo()
+        }catch (e: Exception){
+            Toast.makeText(this, "${e}",Toast.LENGTH_SHORT).show()
+        }
 
         binding.buttonVoltar.setOnClickListener {
             finish()
@@ -80,9 +96,20 @@ class FrequenciaActivity : AppCompatActivity() {
 
     private fun confirmarHost() {
         val pareados = listaPareados()
-        val dbHelper = DBHelper(this)
+        val professor = binding.optionsSpinner.selectedItem.toString()
+        val bluetoothNameId = dbRef.child(professor).toString()
+        dbRef.child(professor).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (disciplinaSnapshot in dataSnapshot.children) {
+                    val bluetoothNameId = disciplinaSnapshot.key.toString()
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(this@FrequenciaActivity, "${databaseError.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
         for (dispositivo in pareados) {
-            if (dbHelper.isProfessorMac(dispositivo.address.toString())) {
+            if (bluetoothNameId == dispositivo.name.toString()) {
                 Toast.makeText(this, "Pareamento com Host Confirmado", Toast.LENGTH_SHORT).show()
                 if (ActivityCompat.checkSelfPermission(
                         this,
@@ -98,7 +125,6 @@ class FrequenciaActivity : AppCompatActivity() {
                     // for ActivityCompat#requestPermissions for more details.
                     return
                 }
-                dbHelper.inserirFrequencia(obterNomeTabelaFrequencia(), dispositivo.name, dispositivo.address)
                 binding.status.text = "Frequência Realizada"
                 binding.confirmButton.setBackgroundResource(R.drawable.baseline_bluetooth_connected_24_white)
                 binding.status.setBackgroundResource(R.drawable.bg_btn_blue)
@@ -120,5 +146,36 @@ class FrequenciaActivity : AppCompatActivity() {
         val formatoData = SimpleDateFormat("yyyyMMdd_HHmmss")
         val dataAtual = Date()
         return "frequencia_${formatoData.format(dataAtual)}"
+    }
+
+    fun listPeriodo(){
+        try {
+            listagemPeriodo { disciplinasArray ->
+                val adapter = ArrayAdapter(this,
+                    R.layout.simple_list_item_1, disciplinasArray)
+                binding.optionsSpinner.adapter = adapter
+            }
+        } catch (e: Exception) {
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun listagemPeriodo(callback: (Array<String>) -> Unit) {
+        val disciplinasList = mutableListOf<String>()
+        val user = FirebaseAuth.getInstance().currentUser
+        dbRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (disciplinaSnapshot in dataSnapshot.children) {
+                    val disciplinaNome = disciplinaSnapshot.key.toString()
+                    disciplinasList.add(disciplinaNome!!)
+                }
+                callback(disciplinasList.toTypedArray())
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(this@FrequenciaActivity, "${databaseError.message}", Toast.LENGTH_SHORT).show()
+                callback(emptyArray())
+            }
+        })
     }
 }
